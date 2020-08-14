9ca04447e00a2dbd28fceff6c746a30126378acf
SH0909 <poa0909@gmail.com>
2020-08-14 15:30:22 +0900

    비밀번호 암호화,파일 저장

--- ExcelCalculate/calculate/views.py
+++ ExcelCalculate/calculate/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import xlrd
 import pandas as pd
+from datetime import datetime
+from .models import *
 
 def calculate(request):
     file=request.FILES['fileInput']
+    origin_file_name=file.name
+    user_name=request.session['user_name']
+    now_HMS=datetime.today().strftime('%H%M%S')
+    file_upload_name=now_HMS+'_'+user_name+'_'+origin_file_name
+    file.name=file_upload_name
+    document=Document(user_upload_file=file)
+    document.save()
     df=pd.read_excel(file,sheet_name='Sheet1',header=0)
     #grade별 value 리스트 만들기
     grade_dic={}
@@ -27,9 +36,9 @@ def calculate(request):
     grade_list.sort()
     for key in grade_list:
         print("# grade:",key)
-        print("min:",grade_calculate_dic[key]['min'],end='')
-        print("/ max:",grade_calculate_dic[key]['max'],end='')
-        print("/ avg:",grade_calculate_dic[key]['avg'],end='\n\n')
+        #print("min:",grade_calculate_dic[key]['min'],end='')
+        #print("/ max:",grade_calculate_dic[key]['max'],end='')
+        #print("/ avg:",grade_calculate_dic[key]['avg'],end='\n\n')
     #이메일 주소 도메인별 인원 구하기
     email_domain_dic={}
     for i in range(total_row_num):
@@ -39,9 +48,9 @@ def calculate(request):
             email_domain_dic[email_domain]=1
         else:
             email_domain_dic[email_domain]+=1
-    print("email 도메인 별 사용인원")
-    for key in email_domain_dic.keys():
-        print("#",key,": ",email_domain_dic[key],"명")
+    #print("email 도메인 별 사용인원")
+    #for key in email_domain_dic.keys():
+        #print("#",key,": ",email_domain_dic[key],"명")
     grade_calculate_dic_to_session={}
     for key in grade_list:
         grade_calculate_dic_to_session[int(key)]={}
