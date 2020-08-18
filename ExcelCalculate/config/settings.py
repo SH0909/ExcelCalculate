61ce2c85bb0e2530037f2a18cdf831cc63be4189
SH0909 <48480810+SH0909@users.noreply.github.com>
2020-08-19 02:14:48 +0900

    Update settings.py

--- ExcelCalculate/config/settings.py
+++ ExcelCalculate/config/settings.py
@@ -31,8 +31,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='poa0909@gmail.com'
-EMAIL_HOST_PASSWORD='19753wmf@'
+EMAIL_HOST_USER=
+EMAIL_HOST_PASSWORD=
 
 # Application definition
 
